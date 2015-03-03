2457acb30dfe751e104d40082b4a2908d0b2db04
Mark Stephens <mr.morphic@gmail.com>
2015-03-03 13:23:39 +1300

    add permission check to build task

--- code/RedirectedURLExpiryTask.php
+++ code/RedirectedURLExpiryTask.php
@@ -6,7 +6,16 @@
 class RedirectedURLExpiryTask extends BuildTask {
 	public $description = 'Remove expired redirector URL rules';
 
+	public function init() {
+		parent::init();
+		
+		if(!Permission::check('ADMIN')) {
+			return Security::permissionFailure($this);
+		}
+	}
+
 	public function run($request) {
+
 		// @todo if RedirectedURL is extended to support other expiry semantics, this should not filter in SQL.
 		$expired = DataObject::get("RedirectedURL", "\"ExpiryDate\" is not null and \"ExpiryDate\" <= CURDATE()");
 		foreach ($expired as $e) {
